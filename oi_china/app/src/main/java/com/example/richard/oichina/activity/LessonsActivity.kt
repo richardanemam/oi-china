0eddd893e934327626a8157914386d40ab5cb6d1
Richard <richardanemam@gmail.com>
2018-11-11 02:34:28 -0200

    Toolbar da lessons activity refeita

--- oi_china/app/src/main/java/com/example/richard/oichina/activity/LessonsActivity.kt
+++ oi_china/app/src/main/java/com/example/richard/oichina/activity/LessonsActivity.kt
@@ -1,10 +1,10 @@
 package com.example.richard.oichina.activity
 
 import android.os.Bundle
-import android.support.design.widget.AppBarLayout
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import com.example.richard.oichina.R
 import com.example.richard.oichina.adapter.ViewPagerAdapter
 import com.example.richard.oichina.fragments.FragmentBasic
@@ -17,7 +17,7 @@ import com.example.richard.oichina.fragments.FragmentIntermediate
 class LessonsActivity : AppCompatActivity() {
 
     private lateinit var tabLayout: TabLayout
-    private lateinit var appBarLayout: AppBarLayout
+    private lateinit var toolBar: Toolbar
     private lateinit var viewPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,13 +25,20 @@ class LessonsActivity : AppCompatActivity() {
         setContentView(R.layout.lessons)
 
         setUpViews()
+        setUpToolBar()
         addingFragments()
     }
 
     private fun setUpViews() {
         tabLayout = findViewById(R.id.tabLayout_id)
-        appBarLayout = findViewById(R.id.toolbar_id)
+        toolBar = findViewById(R.id.toolbar_id)
         viewPager = findViewById(R.id.viewpager_id)
+    }
+
+    private fun setUpToolBar() {
+        setSupportActionBar(toolBar)
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     private fun addingFragments() {
@@ -39,7 +46,6 @@ class LessonsActivity : AppCompatActivity() {
 
         viewPagerAdapter.addFragment(FragmentBasic(), "Básico")
         viewPagerAdapter.addFragment(FragmentIntermediate(), "Intermediário")
-        //viewPagerAdapter.addFragment(FragmentAdvanced(), "先进")
         setUpAdapter(viewPagerAdapter)
     }
 
